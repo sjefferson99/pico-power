15543ca2a7b5a6cf60511dca846000a1d8de5252
Stephen Jefferson <stephen@sjefferson.co.uk>
2022-11-17 17:40:56 +0000

    Refactored into class

--- main.py
+++ main.py
@@ -1,52 +1,5 @@
-from utime import sleep_ms
-from machine import Pin
+from relay_board import relay_board
 
-#init relays
-relay1 = Pin(18, Pin.OUT)
-relay1.value(0) #Ensure power on
-relay2 = Pin(19, Pin.OUT)
-relay2.value(0) #Ensure power on
-relay3 = Pin(20, Pin.OUT)
-relay3.value(0) #Ensure power on
-relay4 = Pin(21, Pin.OUT)
-relay4.value(0) #Ensure power on
+relays = relay_board()
 
-relays = [relay1, relay2, relay3, relay4]
-
-#init LED
-led = Pin("LED", Pin.OUT)
-led.on()
-sleep_ms(50)
-led.off()
-
-#init vars
-rundemo = False
-
-def relay_on_off(relay: Pin, duration_ms: int = 1000):
-    """Connects common to NO for specified duration in ms then reverts to common connected to NC"""
-    relay.value(1)
-    sleep_ms(duration_ms)
-    relay.value(0)
-
-def relay_off_on(relay: Pin, duration_ms: int = 1000):
-    """Connects common to NC for specified duration in ms then reverts to common connected to NO"""
-    relay.value(0)
-    sleep_ms(duration_ms)
-    relay.value(1)
-
-def demo():
-    x = 0
-    while x < 10:
-        led.on()
-        sleep_ms(100)
-        led.off()
-        sleep_ms(100)
-        x += 1
-    
-    for relay in relays:
-        relay_on_off(relay)
-        sleep_ms(500)
-
-if rundemo:
-    demo()
-    
+relays.demo()
---
+++ relay_board.py
@@ -0,0 +1,39 @@
+from utime import sleep_ms
+from machine import Pin
+
+class relay_board:
+    def __init__(self) -> None:
+        self.pin_mapping = {1: 18, 2: 19, 3: 20, 4: 21}
+        self.relays = {}
+        #Build pin objects and set known state (off)
+        x = 1
+        p = 18
+        while x <= 4:
+            self.relays[x] = Pin(p, Pin.OUT)
+            self.relays[x].value(0) #set NO to off
+            x += 1
+            p += 1
+
+        #init LED
+        self.led = Pin("LED", Pin.OUT)
+        self.led.on()
+        sleep_ms(50)
+        self.led.off()
+
+    def relay_toggle(self, relay: int, duration_ms: int = 1000, initial_value: int=1) -> None:
+        """For specified relay, connects common to intiial value terminal for specified duration in ms then toggles to the opposite terminal
+        Initial value:
+        1: Common connected to NO
+        0: Common connected to NC
+        """
+        self.relays[relay].value(initial_value)
+        sleep_ms(duration_ms)
+        self.relays[relay].toggle()
+
+    def demo(self) -> None:
+    
+        x = 1
+        while x <= 4:
+            self.relay_toggle(x, 100, 1)
+            sleep_ms(200)
+            x += 1
