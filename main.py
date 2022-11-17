d3ccc146bfb5812fca67c8498e07fcf01bf42b5d
Stephen Jefferson <stephen@sjefferson.co.uk>
2022-11-17 16:32:59 +0000

    Basic functions

--- main.py
+++ main.py
@@ -13,13 +13,40 @@ relay4.value(0) #Ensure power on
 
 relays = [relay1, relay2, relay3, relay4]
 
-def interrupt_power(relay, ms=1000):
-    relay.value(1) # Disconnect power
-    sleep_ms(ms)
-    relay.value(0) # Reconnect power
+#init LED
+led = Pin("LED", Pin.OUT)
+led.on()
+sleep_ms(50)
+led.off()
 
-for relay in relays:
-    interrupt_power(relay)
-    sleep_ms(500)
+#init vars
+rundemo = False
 
-#interrupt_power(relays[3])
+def relay_on_off(relay: Pin, duration_ms: int = 1000):
+    """Connects common to NO for specified duration in ms then reverts to common connected to NC"""
+    relay.value(1)
+    sleep_ms(duration_ms)
+    relay.value(0)
+
+def relay_off_on(relay: Pin, duration_ms: int = 1000):
+    """Connects common to NC for specified duration in ms then reverts to common connected to NO"""
+    relay.value(0)
+    sleep_ms(duration_ms)
+    relay.value(1)
+
+def demo():
+    x = 0
+    while x < 10:
+        led.on()
+        sleep_ms(100)
+        led.off()
+        sleep_ms(100)
+        x += 1
+    
+    for relay in relays:
+        relay_on_off(relay)
+        sleep_ms(500)
+
+if rundemo:
+    demo()
+    
