b65cb03290c9cb27905aa0658dc53ec0f462fe95
Stephen Jefferson <stephen@sjefferson.co.uk>
2022-11-18 18:05:25 +0000

    Framework for processing web requests

--- relay_board.py
+++ relay_board.py
@@ -4,6 +4,7 @@ from machine import Pin
 class relay_board:
     """
     Creates abstraction for the pi hut 4 opto relay board for Pico W - Maps relays to GP18-21.
+    Provides URL parsing and content for a webserver
     Execute demo() for a board self test
     """
     def __init__(self) -> None:
@@ -17,7 +18,23 @@ class relay_board:
             self.relays[x].value(0) #set NO to off
             x += 1
             p += 1
-
+        
+        self.base_relay_content = """
+            <p>
+            Use the following endpoints to drive the pico relays:
+            <ul>
+            <li>Switch relay on (NO switched to common) - /relay/switch/{relay number (1-4)}/on</li>
+            <li>Switch relay off (NC switched to common) - /relay/switch/{relay number (1-4)}/off/</li>
+            <li>Toggle relay on (NO switched to common) then off with a specified duration period - /relay/toggle/{relay number (1-4)}/on/{duration in ms}</li>
+            <li>Toggle relay off (NC switched to common) then on with a specified duration period - /relay/toggle/{relay number (1-4)}/off/{duration in ms}</li>
+            </p>
+            """
+        self.unknown_url_content = """
+        <p>
+        That URL is not recognised.
+        </p>
+        """
+        
     def relay_toggle(self, relay: int, duration_ms: int = 1000, initial_value: int=1) -> None:
         """For specified relay, connects common to intiial value terminal for specified duration in ms then toggles to the opposite terminal
         Initial value:
@@ -43,3 +60,26 @@ class relay_board:
             self.relay_toggle(x, 100, 1)
             sleep_ms(200)
             x += 1
+    
+    def parse_web_request(self, request_data: str) -> str:
+        """Takes web request data from a client determined to be in the
+        "/relay" page space, parses the request data, executes any appropriate
+        relay actions and returns the appropriate content to serve to the web
+        client"""
+        
+        relay_url = request_data.find('/relay ')
+        action_relay_url = request_data.find('/relay/')
+
+        if relay_url == 6:
+            print("Relay base URL")
+            content = self.base_relay_content
+
+        elif action_relay_url == 6:
+            print("Relay action URL")
+            content = "Some kind of relay action"
+
+        else:
+            print("Unknown relay URL")
+            content = self.unknown_url_content + self.base_relay_content
+
+        return content
